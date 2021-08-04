265a0f6f4dc41ad6c6b009f66ee4c4e7f07e605f
Kevin Tsang <niveksys@gmail.com>
2021-08-04 23:47:01 +0800

    Custom Hook: useEffectAfterMount

--- showcase/src/patterns/06.js
+++ showcase/src/patterns/06.js
@@ -1,4 +1,10 @@
-import React, { useState, useLayoutEffect, useCallback } from "react";
+import React, {
+  useState,
+  useLayoutEffect,
+  useCallback,
+  useRef,
+  useEffect,
+} from "react";
 import mojs from "mo-js";
 import styles from "./index.css";
 
@@ -135,6 +141,19 @@ const useClapState = (initialState = INITIAL_STATE) => {
   return [clapState, updateClapState];
 };
 
+/**
+ * custom useEffectAfterMount hook
+ */
+const useEffectAfterMount = (cb, deps) => {
+  const componentJustMounted = useRef(true);
+  useEffect(() => {
+    if (!componentJustMounted.current) {
+      return cb();
+    }
+    componentJustMounted.current = false;
+  }, deps);
+};
+
 const MediumClap = () => {
   const [clapState, updateClapState] = useClapState();
   const { count, countTotal, isClicked } = clapState;
@@ -147,17 +166,16 @@ const MediumClap = () => {
     clapTotalEl: clapTotalRef,
   });
 
-  const handleClapClick = () => {
+  useEffectAfterMount(() => {
     animationTimeline.replay();
-    updateClapState();
-  };
+  }, [count]);
 
   return (
     <button
       ref={setRef}
       data-refkey="clapRef"
       className={styles.clap}
-      onClick={handleClapClick}
+      onClick={updateClapState}
     >
       <ClapIcon isClicked={isClicked} />
       <ClapCount count={count} setRef={setRef} />
